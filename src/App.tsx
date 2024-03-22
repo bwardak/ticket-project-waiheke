5e21c6d0d6be7587a8c9ad362e9053022f424de5
bwardak <baheer.wardak06@gmail.com>
2024-03-22 16:08:43 +0000

    Created template of tracker in app file. Created components for employees and counter.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './app.scss'
+
+function App() {
+  const heading = "Ticket Tracker"
+
+  return (
+    <div className='tracker-container'>
+      <h1 className='heading'>{heading}</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/employee/employee.tsx
@@ -0,0 +1,7 @@
+import team from "../../data/team";
+import "./employee.scss";
+
+type Employees = {
+  name: string;
+  role: string
+}
